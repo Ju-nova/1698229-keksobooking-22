fca73444edd3051b66d372c56f7e2bcdf6db1bed
Yulia <46112108+Ju-nova@users.noreply.github.com>
2021-01-31 22:21:12 +0300

    add random  function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно.
-const getRandomInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min);
-};
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+// и возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+const getRandomNumber = (min, max, numberSymbols) => {
+  const preResult = (Math.random() * (max - min) + min);
+  if (numberSymbols){
+    return Number(preResult.toPrecision(numberSymbols));
+  }
 
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getRandomFractional(min, max, numberSymbols) {
-  return Number((Math.random() * (max - min) + min).toPrecision(numberSymbols));
-}
+  return Math.floor(preResult);
+};
 
